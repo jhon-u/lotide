db0ecc50a792edd93d66ce8bbc32de3c8fa61076
Jhon Useche <jhon.useche@telus.com>
2022-11-15 16:59:18 -0700

    Implement eqArrays functionality

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arrayA, arrayB) {
+  for (let index in arrayA) {
+    if (arrayA[index] !== arrayB[index]) {
+      return false;
+    }
+  }
+  return true
+}
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
