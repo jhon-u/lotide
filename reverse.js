ecdbd6607428e488e4795079da6d4ae9e62e39d5
Jhon Useche <jhon.useche@telus.com>
2022-11-14 18:29:12 -0700

    Add reverse function file

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const reverse = function(array) {
+  for (let i = 0; i < array.length; i ++) {
+    let reversed = '';
+    for(let j = array[i].length - 1; j >= 0; j--) {
+      reversed += array[i][j]
+    }
+    console.log(reversed)
+  }
+}
+
+const args = process.argv.slice(2);
+reverse(args);
