d85a3c6c0f35391d8134e2690819fbec995f85ce
Jhon Useche <jhon.useche@telus.com>
2022-11-18 20:30:11 -0700

    Refactor eqObjects function to check objects Recursively

--- eqObjects.js
+++ eqObjects.js
@@ -6,43 +6,31 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) return false;
-
-  for (let index in arrayA) {
-    if (arrayA[index] !== arrayB[index]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
-
   for (let key in object1) {
-    if (Array.isArray(object1[key])) {
-      if (!(Array.isArray(object2[key]))) return false;
-      if (!eqArrays(object1[key], object2[key])) return false;
-    } else if (object1[key] !== object2[key]) return false;
+    if (typeof object1[key] === 'object' && !Array.isArray(object1[key])) {
+      return eqObjects(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
   }
+
   return true;
 };
 
 // TEST CODE
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+const ab = { a: { z: 1 }, b: 2 };
+const ac = { a: { z: 1 }, b: 2 };
+assertEqual(eqObjects(ab, ac), true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+const ba = { a: { y: 0, z: 1 }, b: 2 };
+const bc = { a: { z: 1 }, b: 2 };
+assertEqual(eqObjects(ba, bc), false); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+const cb = { a: { y: 0, z: 1 }, b: 2 };
+const cd = { a: 1, b: 2 };
+assertEqual(eqObjects(cb, cd), false); // => false
