0fb19535299d74106ccad4a4a9d37f24f57e2a8c
Jhon Useche <jhon.useche@telus.com>
2022-11-21 10:30:40 -0700

    Refactor the middle.js file to use modules.

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) return false;
-
-  for (let index in arrayA) {
-    if (arrayA[index] !== arrayB[index]) {
-      return false;
-    }
-  }
-  
-  return true;
-};
-
-const assertArraysEqual  = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB)) {
-    console.log(`✅✅✅ Assertion Passed: ${arrayA} === ${arrayB}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arrayA} !== ${arrayB}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) return [];
   
@@ -35,12 +15,4 @@ const middle = function(array) {
   return result;
 };
 
-// TEST CODEse
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
+module.exports = middle;
